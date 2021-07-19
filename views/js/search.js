024e19e7743e87a9701b4a4d52f44095ef42c67c
wxamlzm <zd5024@163.com>
2021-07-19 21:13:38 +0800

    UPDATE:更新search模块

---
+++ views/js/search.js
@@ -0,0 +1,32 @@
+// 获取dom
+let search = document.getElementById('search');
+let btn = document.getElementById('btn');
+let panel = document.getElementById('panel');
+
+// 当点击btn的时候，获取search的内容，匹配“数据库”返回的数据
+// 并且给到对应ID的图片展示在search-panel窗口上
+
+// 监听click事件
+btn.onclick = function(){
+  let searchVal = search.value;
+
+  showPoke(searchVal);
+}
+
+// 输出对应图片
+function showPoke(searchVal){
+  // 清空面板
+  panel.innerHTML = '';
+  // 进行输入数据的判断
+  if (searchVal == ''){
+    panel.innerHTML = '<p class="response">请输入信息</p>'
+  }else if (isNaN(searchVal)){
+    // 为true 证明输入的是文字,匹配名字
+    for(let i = 0; i < data.length; i++){
+      if(searchVal == data[i].name){
+        panel.innerHTML = `<img src='./img/poke/${data[i].id}.png' class='img'>`
+      }
+    }
+  }//待完善
+
+}
